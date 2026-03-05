963adf9b301e226c33fe5310a405ca5c09a6df2c
Matheus <matheus63360@gmail.com>
2026-03-05 09:50:54 -0300

    feat: create profile domain

---
+++ src/domains/profile/profile.content.ts
@@ -0,0 +1,14 @@
+import { Profile } from './profile.model';
+
+export const profileContent: Profile = {
+  name: 'Matheus Ramyres',
+  email: 'matheus63360@mail.com',
+  role: 'Desenvolvedor Full Stack',
+  description:
+    'O Desenvolvedor Fullstack que transforma problemas complexos em soluções simples e escaláveis',
+  subtitle:
+    'Crio aplicações web robustas, dashboards e sistemas com foco em performance, UX e código limpo, utilizando tecnologias modernas do ecossistema JavaScript.',
+  linkedin: 'https://www.linkedin.com/in/matheusramyres',
+  github: 'https://github.com/matheusramyres',
+  avatarUrl: '/avatar.png',
+};
---
+++ src/domains/profile/profile.model.ts
@@ -0,0 +1,10 @@
+export interface Profile {
+  name: string;
+  email: string;
+  role: string;
+  description: string;
+  subtitle: string;
+  linkedin: string;
+  github: string;
+  avatarUrl: string;
+}
