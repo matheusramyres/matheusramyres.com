d2bca68494a6fc03babbc6d479394ad9057b9a19
Matheus <matheus63360@gmail.com>
2026-03-05 09:52:58 -0300

    feat: created Button

---
+++ src/ui/components/Button/index.tsx
@@ -0,0 +1,33 @@
+import clsx from 'clsx';
+
+type Variant = 'primary' | 'secondary';
+
+interface ButtonProps {
+  className?: string;
+  children: React.ReactNode;
+  variant?: Variant;
+}
+
+export function Button({ children, className, variant }: ButtonProps) {
+  const stylesButton = {
+    primary: 'bg-gray-950 text-white hover:bg-zinc-800',
+    secondary:
+      'bg-white text-slate-950 border border-slate-200 hover:bg-[#f2f2f2]',
+  };
+
+  if (!variant) variant = 'primary';
+
+  const styleVariant = stylesButton[variant];
+  return (
+    <button
+      className={clsx(
+        'py-2.75 px-7.75',
+        'rounded-lg font-medium',
+        'cursor-pointer',
+        styleVariant,
+      )}
+    >
+      {children}
+    </button>
+  );
+}
