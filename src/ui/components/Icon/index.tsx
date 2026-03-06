59c11a63ceb552ef328d1115fd944b574dfcdfbb
Matheus <matheus63360@gmail.com>
2026-03-05 21:36:48 -0300

    feat: update Icon

--- src/ui/components/Icon/index.tsx
+++ src/ui/components/Icon/index.tsx
@@ -5,10 +5,11 @@ export type IconName = keyof typeof Icons;
 interface IconProps {
   icon: IconName;
   size?: number;
+  color?: string;
   className?: string;
 }
 
-export function Icon({ icon, size, className }: IconProps) {
+export function Icon({ icon, size, color, className }: IconProps) {
   const IconComponent = Icons[icon];
-  return <IconComponent size={size} />;
+  return <IconComponent size={size} color={color} />;
 }
