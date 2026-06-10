1522c543cca42b912e9bac44ce95855f9648c63d
Matheus <matheus63360@gmail.com>
2026-06-10 12:18:04 -0300

    feat: organize page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
-import Image from "next/image";
+import AboutSection from '@/ui/sections/AboutSection';
+import ContactSection from '@/ui/sections/ContactSection';
+import ExperienceSection from '@/ui/sections/ExperienceSection';
+import ProfileSection from '@/ui/sections/ProfileSection';
+import ProjectsSection from '@/ui/sections/ProjectsSection';
+import SkillsSection from '@/ui/sections/SkillsSection';
 
 export default function Home() {
-  return <div>Matheus</div>;
+  return (
+    <div className="mt-18 px-5 md:px-16 pt-6 md:pt-8.75 ">
+      <ProfileSection />
+      <AboutSection />
+      <SkillsSection />
+      <ExperienceSection />
+      <ProjectsSection />
+      <ContactSection />
+    </div>
+  );
 }
